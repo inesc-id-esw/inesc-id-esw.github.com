fc9b7e22571a762cd6af971675a1147734220329
Sérgio Miguel Fernandes <sergio.fernandes@ist.utl.pt>
2013-02-26 12:57:16 +0000

    Move update-directory-index.sh location so that is does not show up in web pages

--- _includes/update-directory-index.sh
+++ _includes/update-directory-index.sh
@@ -6,22 +6,23 @@
 ## This function sets this script's correct path even if it is run from
 ## another dir
 
-THIS_DIR=
+SCRIPT_DIR=
 function absname() {
   pushd `dirname "$1"` > /dev/null
-  THIS_DIR=`pwd`
+  SCRIPT_DIR=`pwd`
   popd > /dev/null
   return
 }
 
 absname $0
+TARGET_DIR="$SCRIPT_DIR"/../maven-repo
 
-echo "This will overwrite all index.html files on "$THIS_DIR" and subdirectories!"
+echo "This will overwrite all index.html files on "$TARGET_DIR" and subdirectories!"
 echo
 echo "Press <Return> to continue or ctrl-c to abort."
 echo 
 read
-for DIR in $(find "$THIS_DIR" -type d); do
+for DIR in $(find "$TARGET_DIR"/../maven-repo -type d); do
   (
     echo -e "<html>\n<body>\n<h1>Directory listing</h1>\n<hr/>\n<pre>"
     ls -1pa "${DIR}" | grep -v "^\./$" | grep -v "index.html" | awk '{ printf "<a href=\"%s\">%s</a>\n",$1,$1 }' 
